da21721b82837dcb374e6337386a9fd76bd15b65
Bhumi <bhumi@gmail.com>
2026-06-01 14:24:49 +0530

    Add Cloudinary

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
+const { storage } = require('../config/cloudinary');
 
 const {
   createNote,
@@ -12,29 +12,10 @@ const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
 const upload = multer({ storage });
 
-// Upload Note
-router.post(
-  '/',
-  upload.single('file'),
-  protect,
-  createNote
-);
-
-// Get Notes
+router.post('/', upload.single('file'), protect, createNote);
 router.get('/', getNotes);
-
-// Delete Note
 router.delete('/:id', protect, deleteNote);
 
 module.exports = router;
